04127a584526caf8e1909cff0a10915d1cebb72a
Tristan Brown <tgbrown@stanford.edu>
2018-09-30 15:02:55 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,66 @@
 class Owner
-  # code goes here
+  attr_accessor :owner, :cat, :dog, :fish, :pets, :fishes, :cats, :dogs
+  attr_reader :species, :name
+
+  @@all = []
+
+  def initialize(owner)
+    @owner = owner
+    @species = owner
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.species
+    @species = species
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def name=(name)
+    @name=name
+  end
+
+  def self.buy_fish
+  end
+
+  def self.buy_cat
+  end
+
+  def self.buy_dog
+  end
+
+  def self.walk_dog
+  end
+
+  def self.play_with_cats
+  end
+
+  def self.feed_fish
+  end
+
+  def self.sell_pets
+  end
+
+  def self.list_pets
+  end
+
+
+  def pets
+  end
+
 end
