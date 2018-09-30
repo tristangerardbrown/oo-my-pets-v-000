b1140204a5700649ef5fad57df3b560906034ee1
Tristan Brown <tgbrown@stanford.edu>
2018-09-30 16:55:10 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -70,6 +70,12 @@ class Owner
   end
 
   def sell_pets
+    pets.each do |species, animals|
+		 animals.each do |animal|
+		 	animal.mood = "nervous"
+		 end
+		 animals.clear
+		end 
   end
 
   def list_pets
